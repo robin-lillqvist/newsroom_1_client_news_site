d59fd8748531449140201e6756b1e84af52411b7
Philip Gaunitz <fluffen@pgaunitz.se>
2020-03-19 17:48:36 +0100

    populated app class to display articles with a get req, ads stubs NOT working, installed axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-const App = () => {
+class App extends Component {
+  state = {
+    articles: []
+  };
+
+  componentDidMount() {
+    axios.get("/articles").then(response => {
+      this.setState({ articles: response.data.articles });
+      debugger
+    });
+  }
+  render() {
+    let displayArticles;
+    displayArticles = this.state.articles.map(article => {
+      return (
+        <div key={article.id} id="article-list">
+          {article.title}
+          {article.lead}
+        </div>
+      );
+    });
+    return(
+      <>
+      <h1>
+        Berlingo New
+      </h1>
+      {displayArticles}
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/state/store/initialState.js
@@ -0,0 +1,5 @@
+const initialState = {
+	articles: true
+}
+
+export default initialState
