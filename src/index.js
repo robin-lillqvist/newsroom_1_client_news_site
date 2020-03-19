d257e663c269fa2fe7cce14c2f5950d82254fd62
kaylawoodbury <kaylasandoval92@gmail.com>
2020-03-19 19:53:35 +0100

    update index js base url from 300 to 3000

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:300/api/";
+axios.defaults.baseURL = "http://localhost:3000/api/";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
