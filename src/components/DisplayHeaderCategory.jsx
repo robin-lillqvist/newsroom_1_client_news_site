626e465372f80df309b9a675ada9b28b7695f255
kaylawoodbury <kaylasandoval92@gmail.com>
2020-03-21 22:03:05 +0100

    Placing commit before logging out for the night, only a few minor changes to the styling on the DisplayHeaderCategory file

--- src/components/DisplayHeaderCategory.jsx
+++ src/components/DisplayHeaderCategory.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Menu, Segment, Icon } from "semantic-ui-react";
-import { Link } from 'react-router-DOM'
+import { Link } from 'react-router-dom'
 
-const HeaderCategories = () => {
-  const { activeItem } = props
+class HeaderCategories extends Component {
+  state = {
+    activeItem: '',
+  }
 
-  return (
-    <Menu pointing secondary style={{ backgroundColor: "white" }}>
-      <Menu.Item name="Latest News" id="category-latest-news" as={Link} active={props.activeItem === "category-latest-news"} onClick={props.handleItemClick} />
-      <Menu.Item name="Culture" id="category-culture" as={Link} active={props.activeItem === "category-culture"} onClick={props.handleItemClick} />
-      <Menu.Item name="Tech" id="category-tech" as={Link} active={props.activeItem === "category-tech"} onClick={props.handleItemClick} />
-      <Menu.Item name="Food" id="category-food" as={Link} active={props.activeItem === "category-food"} onClick={props.handleItemClick} />
-      <Menu.Item name="Sports" id="category-sports" as={Link} active={props.activeItem === "category-sports"} onClick={props.handleItemClick} />
-    </Menu>
-  );
+  handleItemClick = (e, { name }) => { this.setState({ activeItem: name }) }
+
+  render() {
+    const { activeItem } = this.state
+    let categories = [{ 'name': 'Latest News' }, { 'name': 'Culture' }, { 'name': 'Tech' }, { 'name': 'Food' }, { 'name': 'Sports' }]
+    return (
+      < Menu pointing centered secondary style={{ backgroundColor: "white" }
+      }>
+        {categories.map(category => (
+          <Menu.Item
+            id={category.name}
+            name={category.name}
+            key={category.name}
+            as={Link}
+            to={{ pathname: `/${category.name.toLowerCase()}`, state: { categoryName: `${category.name}` } }}
+            active={activeItem === category.name}
+            onCLick={this.handleItemClick}
+          />
+        ))}
+
+        {/* <Menu.Item name="Latest News" id="category-latest-news" as={Link} active={props.activeItem === "category-latest-news"} onClick={props.handleItemClick} />
+  <Menu.Item name="Culture" id="category-culture" as={Link} active={props.activeItem === "category-culture"} onClick={props.handleItemClick} />
+  <Menu.Item name="Tech" id="category-tech" as={Link} active={props.activeItem === "category-tech"} onClick={props.handleItemClick} />
+  <Menu.Item name="Food" id="category-food" as={Link} active={props.activeItem === "category-food"} onClick={props.handleItemClick} />
+  <Menu.Item name="Sports" id="category-sports" as={Link} active={props.activeItem === "category-sports"} onClick={props.handleItemClick} /> */}
+      </Menu >
+    );
+  }
+
 };
 
 export default HeaderCategories;
