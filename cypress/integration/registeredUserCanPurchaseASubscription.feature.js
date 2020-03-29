252940a40e5d522163f59577e1e9c398c17a2627
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-03-29 17:40:15 +0200

    Adds beginning of test for subscription button

---
+++ cypress/integration/registeredUserCanPurchaseASubscription.feature.js
@@ -0,0 +1,27 @@
+describe ('registered user can purchase a subscription', () => {
+    beforeEach(() => {
+        cy.server();
+        cy.route({
+          method: "GET",
+          url: "**/articles",
+          response: "fixture:articles_list_response.json"
+        });
+        cy.route({
+          method: "GET",
+          url: "**/articles",
+          response: "fixture:articles_list_response.json"
+        });
+        cy.route({
+          method: "GET",
+          url: "**/articles/2",
+          response: "fixture:specific_article_2.json"
+        });
+        cy.visit("/");
+        cy.get("#open-article-2").click();
+    });
+
+    it('by clicking buy subscription', () =>{
+        cy.get('button').contains('Buy Subscription').click()
+    })
+
+})
