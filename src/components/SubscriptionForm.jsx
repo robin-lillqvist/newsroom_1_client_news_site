3e2539a147dcaad179a386b60138d24414e9b134
Blake <blake@futchi.com>
2020-03-29 18:52:43 +0200

    added form fields and user path to subscription purchase form, needs refactoring

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Header, Modal, Form } from "semantic-ui-react";
+import { Header, Modal, Form, Button } from "semantic-ui-react";
 
 const SubscriptionForm = () => {
+  
   return (
-    <Modal open={true}>
+    <Modal trigger={<Button>Make Payment</Button>} closeIcon>
       <Header content="Subscription Form" />
       <Modal.Content>
         <Form id="subscription-form">
             <Form.Field>
-                Name
+            <input placeholder='Name on Card' />
             </Form.Field>
+            <Form.Field>
+            <input placeholder='Card Number' />
+            </Form.Field>
+            <Form.Field>
+            <input placeholder='CVC' />
+            </Form.Field>
+            <Form.Field>
+            <input placeholder='Billing Address' />
+            </Form.Field>
+            <Button positive>Confirm Subscription</Button>
         </Form>
       </Modal.Content>
     </Modal>
