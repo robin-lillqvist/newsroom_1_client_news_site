f8044b31fa47f1faf053a2b9b413e4239c397e3e
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-03-30 09:44:49 +0200

    Adds hook useDispatch for flash message
    Co-authored-by: kayla kaylasandoval92@gmail.com
    Co-authored-by: robin robin.lillqvist@gmail.com

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Header, Modal, Form, Button, Label } from "semantic-ui-react";
-import { CardNumberElement, CardExpiryElement, CardCVCElement, injectStripe } from "react-stripe-elements";
-import axios from 'axios'
+import {
+  CardNumberElement,
+  CardExpiryElement,
+  CardCVCElement,
+  injectStripe
+} from "react-stripe-elements";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+
 const SubscriptionForm = props => {
-  const submitPayment = async (event) => {
-    event.preventDefault()
-    let stripeResponse = await props.stripe.createToken()
-    let token = stripeResponse.token.id
-    let paymentStatus = await axios.post('**/subscriptions', { stripeToken: token})
-    if (paymentStatus.data.status  === 'paid')
-    props.dispatch({type: 'FLASH_MESSAGE', payload: {flashMessage: 'Thank you for your business!'}})
-  }
-    return (
+  const dispatch = useDispatch();
+  const submitPayment = async event => {
+    event.preventDefault();
+    let stripeResponse = await props.stripe.createToken();
+    let token = stripeResponse.token.id;
+    let paymentStatus = await axios.post("**/subscriptions", {
+      stripeToken: token
+    });
+    if (paymentStatus.data.status === "paid")
+      dispatch({
+        type: "FLASH_MESSAGE",
+        payload: { flashMessage: "Thank you for your business!" }
+      });
+  };
+  return (
     // <Modal trigger={<Button>Make Payment</Button>} closeIcon>
     //   <Header content="Subscription Form" />
     //   <Modal.Content>
@@ -32,7 +45,6 @@ const SubscriptionForm = props => {
     //   </Modal.Content>
     // </Modal>
 
-
     <Form id="payment-form">
       <Header textAlign="center" level="4">
         Payment Form
@@ -48,8 +60,10 @@ const SubscriptionForm = props => {
         onClick={event => {
           submitPayment(event);
         }}
-      >Submit Payment</Button>
-    </Form> 
+      >
+        Submit Payment
+      </Button>
+    </Form>
   );
 };
 
