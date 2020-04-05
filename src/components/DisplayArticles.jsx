fdcfccb8c5bd992541b3647159b22881b48354e5
robin-lillqvist <robin.lillqvist@gmail.com>
2020-04-05 15:01:27 +0200

    added images to the articles, changed premium message color

--- src/components/DisplayArticles.jsx
+++ src/components/DisplayArticles.jsx
@@ -1,56 +1,78 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Grid, Header, Image, Button } from "semantic-ui-react";
-import { fetchSingleArticle } from "../state/actions/articleActions";
-import { bindActionCreators } from "redux";
-import { useTranslation } from "react-i18next";
+import React from 'react'
+import { connect } from 'react-redux'
+import { Card, Icon, Grid, Header, Image, Button } from 'semantic-ui-react'
+import { fetchSingleArticle } from '../state/actions/articleActions'
+import { bindActionCreators } from 'redux'
+import { useTranslation } from 'react-i18next'
+import imagePlaceholder from '../images/placeholder.png'
 
 const DisplayArticles = props => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
   const singleArticle = articleID => {
-    props.fetchSingleArticle(articleID);
-  };
-  let articles;
+    props.fetchSingleArticle(articleID)
+  }
+  let articles
+  let image
   if (props.categoryName) {
     articles = props.articles.filter(article => {
-      return article.category === props.categoryName && article;
-    });
+      return article.category === props.categoryName && article
+    })
   } else {
-    articles = props.articles;
+    articles = props.articles
   }
+
   let articleDisplay = articles.map(article => {
+    if (article.image) {
+      image = article.image
+    } else {
+      image = imagePlaceholder
+    }
     return (
-      <Grid key={article.id} id={`article-${article.id}`} align="center">
-        <Grid.Column>
-          <Image src={article.image} size="medium" />
-          <Header>{article.title}</Header>
-          <p>{article.lead}</p>
-          <Button
-            id={`open-article-${article.id}`}
-            onClick={() => singleArticle(article.id)}
+      <Grid centered>
+        <Grid.Column mobile={12} tablet={12} computer={8} largeScreen={8} widescreen={6}>
+          <Card
             key={article.id}
+            id={`article-${article.id}`}
+            align='center'
+            fluid
           >
-            {t("article.read-more")}
-          </Button>
+            <Image src={image} wrapped ui={false} size='medium' />
+            <Card.Content>
+              <Card.Header>{article.title}</Card.Header>
+              <Card.Description>
+                <span className='date'>{article.lead}</span>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <Button
+                id={`open-article-${article.id}`}
+                onClick={() => singleArticle(article.id)}
+                key={article.id}
+              >
+                {' '}
+                {t('article.read-more')}
+              </Button>
+            </Card.Content>
+          </Card>
         </Grid.Column>
       </Grid>
-    );
-  });
+    )
+  })
 
-  return <div id="article-list">{articleDisplay}</div>;
-};
+  return <div id='article-list'>{articleDisplay}</div>
+}
 
 const mapStateToProps = state => {
   return {
     articles: state.articles,
     categoryName: state.categoryName
-  };
-};
+  }
+}
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchSingleArticle: bindActionCreators(fetchSingleArticle, dispatch)
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(DisplayArticles);
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayArticles)
--- src/components/DisplaySingleArticle.jsx
+++ src/components/DisplaySingleArticle.jsx
@@ -1,79 +1,88 @@
-import React from "react";
-import { connect } from "react-redux";
-import { useSelector } from "react-redux";
-import { Button, Header, Icon, Image, Container } from "semantic-ui-react";
-import { useTranslation } from "react-i18next";
+import React from 'react'
+import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { Button, Header, Icon, Image, Container } from 'semantic-ui-react'
+import { useTranslation } from 'react-i18next'
+import imagePlaceholder from '../images/placeholder.png'
 
 const DisplaySingleArticle = props => {
-  const { t } = useTranslation();
-  const premiumUser = useSelector(state => state.premiumUser);
-  const userEmail = useSelector(state => state.userEmail);
-  let articleDetails;
-  let articlePremium = props.singleArticle.premium;
-  let article = props.singleArticle;
-  let premiumMessage = "";
+  const { t } = useTranslation()
+  const premiumUser = useSelector(state => state.premiumUser)
+  const userEmail = useSelector(state => state.userEmail)
+  let articleDetails
+  let articlePremium = props.singleArticle.premium
+  let article = props.singleArticle
+  let image
+  if (article.image) {
+    image = article.image
+  } else {
+    image = imagePlaceholder
+  }
+  let premiumMessage = ''
 
   if (
     premiumUser === false &&
     articlePremium === true &&
     userEmail !== undefined
   ) {
-    article.content = article.content.substring(0, 200) + "...";
+    article.content = article.content.substring(0, 200) + '...'
     premiumMessage = (
-      <p id="premium-message">
-        {t("article.premium-message")}
+      <p id='premium-message'>
+        {t('article.premium-message')}
         <Button
           positive
           onClick={() =>
             props.dispatch({
-              type: "SHOW_SUBSCRIPTION",
+              type: 'SHOW_SUBSCRIPTION',
               payload: { showSubscription: true }
             })
           }
         >
-          {t("article.buy-subscription")}
+          {t('article.buy-subscription')}
         </Button>
       </p>
-    );
+    )
   } else if (userEmail === undefined && articlePremium === true) {
-    article.content = article.content.substring(0, 200) + "...";
+    article.content = article.content.substring(0, 200) + '...'
     premiumMessage = (
-      <p id="premium-message">{t("article.login-for-premium")}</p>
-    );
+      <p id='premium-message'>{t('article.login-for-premium')}</p>
+    )
   }
 
   articleDetails = (
+    
     <div id={`article-${article.id}`}>
-      <Image size="medium" src={article.image} />
       <br />
-      <Container key={article.id} align="center">
-        <Header as="h1" id="article-title">
+      <Container key={article.id} align='center'>
+      
+        <Header as='h1' id='article-title'>
           {article.title}
         </Header>
-        <Header as="h4" id="article-lead">
+        <Image src={image}/>
+        <Header as='h4' id='article-lead'>
           {article.lead}
         </Header>
-        <p id="article-content">{article.content}</p>
+        <p id='article-content'>{article.content}</p>
         {premiumMessage}
         <Button
-          id="back-button"
-          onClick={() => props.dispatch({ type: "BACK_TO_ARTICLE_LIST" })}
+          id='back-button'
+          onClick={() => props.dispatch({ type: 'BACK_TO_ARTICLE_LIST' })}
           key={article.id}
         >
-          <Icon name="chevron left" />
-          {t("article.back")}
+          <Icon name='chevron left' />
+          {t('article.back')}
         </Button>
       </Container>
     </div>
-  );
+  )
 
-  return <div id="single-article">{articleDetails}</div>;
-};
+  return <div id='single-article'>{articleDetails}</div>
+}
 
 const mapStateToProps = state => {
   return {
     singleArticle: state.singleArticle
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps)(DisplaySingleArticle);
+export default connect(mapStateToProps)(DisplaySingleArticle)
